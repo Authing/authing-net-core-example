2acf5de20e4b8a4aee475cecdfdd4a1f1edc1ff8
su <recallsufuture@gmail.com>
2020-07-15 10:34:14 +0800

    feat: 完善 demo

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace AuthingNetCoreExample.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public HomeController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        [HttpGet]
+        public void Home()
+        {
+            string clientId = Configuration["Authing:OidcClientId"];
+            string redirectUrl = Configuration["Authing:OidcRedirectUrl"];
+            Response.Redirect($"https://authing-net-sdk-demo.authing.cn/oauth/oidc/auth?client_id={clientId}&redirect_uri={redirectUrl}&scope=openid profile&response_type=code&state=jacket");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace AuthingNetCoreExample
 {
@@ -25,6 +29,29 @@ namespace AuthingNetCoreExample
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            string domain = $"https://{Configuration["Authing:Domain"]}.authing.cn/oauth/oidc";
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = domain;
+                options.Audience = Configuration["Authing:OidcClientId"];
+            });
+
+            services.AddAuthorization(options =>
+            {
+                // 可以在此处增加更精细的权限定义
+                // 然后 Controller 中使用 [Authorize("read:messages")] 即可
+                options.AddPolicy("read:messages", policy => policy.Requirements.Add(new HasScopeRequirement("read:messages", domain)));
+            });
+
+            // register the scope authorization handler
+            services.AddSingleton<IAuthorizationHandler, HasScopeHandler>();
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +64,7 @@ namespace AuthingNetCoreExample
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
